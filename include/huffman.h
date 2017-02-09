1ac98f70e424c50d6ca42a041f5d8b7534c8afeb
Kynan <kfjustis@gmail.com>
2017-02-09 12:21:03 -0600

    Added node-type array sorting

--- include/huffman.h
+++ include/huffman.h
@@ -3,11 +3,15 @@
 
 #include <time.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <limits.h> // for UCHAR_MAX
 
-#define DIST_SIZE UCHAR_MAX + 1
+#define DIST_SIZE (UCHAR_MAX + 1)
 
 // Generates a random distribution
 int generate_dist_array(float* buffer, unsigned int size);
+
+// Prints an entire array of floating point values
+int print_dist_array(float* dist_array, unsigned int size);
 
 #endif
--- src/dic2.c
+++ src/dic2.c
@@ -16,15 +16,21 @@ int float_comparator (const void* a, const void* b) {
 	return (f1 > f2) - (f1 < f2);
 }
 
+int node_comparator (const void* a, const void* b) {
+	node_t n1 = *(const node_t*) a;
+	node_t n2 = *(const node_t*) b;
+	return (n1.px > n2.px) - (n1.px < n2.px);
+}
+
 int main(int argc, char *argv[]) {
 	if (argc != 2) {
 		printf("Invalid input!\nUsage: %s <input file>\n", argv[0]);
 		return -1;
 	}
 
-	// Run huffman first
+	// run huffman first
 
-	// Create array with random character distributions
+	// create array with random character distributions
 	float dist[DIST_SIZE] = {1.0};
 	int i = 0;
 	for (i = 0; i < DIST_SIZE; i++) {
@@ -36,6 +42,35 @@ int main(int argc, char *argv[]) {
 		return -1;
 	}
 
+	if (print_dist_array(dist, DIST_SIZE) != 0) {
+		printf("Invalid array or size; could not print!\n");
+		return -1;
+	}
+
+	// generate node array from dist array
+	node_t* nodes = calloc(DIST_SIZE, sizeof(node_t));
+	for (i = 0; i < DIST_SIZE; i++) {
+		nodes[i].data = i;
+		nodes[i].px = dist[i];
+	}
+
+	// print from node array
+	for (i = 0; i < DIST_SIZE; i++) {
+		printf("i: %d :: c: %c :: px: %f\n", i, nodes[i].data, nodes[i].px);
+	}
+
+	// qsort and print random distributions
+	qsort(nodes, DIST_SIZE, sizeof(node_t), node_comparator);
+
+	// print after sort
+	printf("\n--AFTER SORT--\n");
+	for (i = 0; i < DIST_SIZE; i++) {
+		printf("i: %d :: c: %c :: px: %f\n", i, nodes[i].data, nodes[i].px);
+	}
+
+	// generate huffman tree from sorted array here
+
+	/*
 	// Create distribution array from file
 	float file_dist[DIST_SIZE] = {0};
 	for (i = 0; i < DIST_SIZE; i++) { // Init probabilities to 1.0
@@ -46,17 +81,9 @@ int main(int argc, char *argv[]) {
 		printf("Failed to make distribution from file!\n");
 		return -1;
 	}	
-	
-	// qsort and print random distributions
-	qsort(dist, DIST_SIZE, sizeof(float), float_comparator);
+	*/
 
-	printf("\n");
-	printf("--GENERATED DISTRIBUTIONS--\n");
-	if (print_char_dist_array(dist) != 0) {
-		printf("Failed to print sorted char dist from file!\n");
-		return -1;
-	}
-
+	/*
 	// qsort and print file distributions
 	qsort(file_dist, DIST_SIZE, sizeof(float), float_comparator);
 
@@ -65,13 +92,7 @@ int main(int argc, char *argv[]) {
 	if (print_char_dist_array(file_dist) != 0) {
 		printf("Failed to print sorted char dist from file!\n");
 		return -1;
-	}
-
-	//////////////////////////
-	/// TESTING
-	//////////////////////////
-	node_t* test = create_node('A', 2.2);
-	printf("test: %c :: %f\n", test->data, test->px);
-	destroy_tree(test);
+	}*/
+	
 	return 0;
 }
